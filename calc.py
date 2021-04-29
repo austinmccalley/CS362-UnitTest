4fba4fa5c638c0b9fbf8a00997285c061603669a
Austin McCalley <austin@austinmccalley.com>
2021-04-29 16:40:18 -0700

    Calc file

---
+++ calc.py
@@ -0,0 +1,34 @@
+def is_integer(s):
+    negative = True if s[:1] == '-' else False
+    if negative:
+        if len(s) == 1:
+            return False
+        s = s[1:]
+        for c in s:
+            if not (c >= '0' and c <= '9'):
+                return False
+        return True
+    else:
+        for c in s:
+            if not(c >= '0' and c <= '9'):
+                return False
+        return True
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mult(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+  
+  
+
